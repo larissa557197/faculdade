37f86ac7c443e9927d3791071fe468ebc996ae16
Larissa Muniz <larissampmuniz@gmail.com>
2025-05-08 17:16:46 -0300

    feat: adicionar método para buscar categorias por usuário no CategoryRepository

--- 1° SEMESTRE/Java/fin-money-api/src/main/java/br/com/fiap/fin_money_api/repository/CategoryRepository.java
+++ 1° SEMESTRE/Java/fin-money-api/src/main/java/br/com/fiap/fin_money_api/repository/CategoryRepository.java
@@ -7,8 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.fiap.fin_money_api.model.Category;
 import br.com.fiap.fin_money_api.model.User;
 
+
+// interface que representa o repositório de categorias
+// estende JpaRepository para fornecer acesso a diversos métodos prontos, como save, findAll, findById, delete, etc.
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    // método personalizado para buscar todas as categorias associadas a um usuário específico
+    // o Spring Data JPA entende esse nome de método e automaticamente cria a consulta baseada nele
     List<Category> findByUser(User user);
     
     
