e8d786ee1f52170a6aed9c580e9973d9e76ee87d
Larissa Muniz <larissampmuniz@gmail.com>
2025-05-08 17:07:03 -0300

    feat: criar entidade Category com validações e relacionamento com User

--- 1° SEMESTRE/Java/fin-money-api/src/main/java/br/com/fiap/fin_money_api/model/Category.java
+++ 1° SEMESTRE/Java/fin-money-api/src/main/java/br/com/fiap/fin_money_api/model/Category.java
@@ -1,5 +1,7 @@
 package br.com.fiap.fin_money_api.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,18 +21,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Category {
+
+    //define o campo 'id' com chave primária da tabela, com geração automática
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    // valida que o campo 'name' não pode estar em branco e deve ter no mínimo 3 caracteres
     @NotBlank(message = "Não pode estar em branco")
     @Size(min = 3)
     private String name;
     
+    // valida que o ícone comece com uma letra maiúscula e não pode estar em branco
     @NotBlank(message = "Não pode estar em branco")
     @Pattern(regexp = "^[A-Z].*", message = "Deve começar com letra maiúscula")
     private String icon;
-
+    // define um relacionamento MUITOS-pra-UM com a entidade User (muitas categorias para um usuário)
     @ManyToOne
+    @JsonIgnore // ignora o campo 'user' na serialização JSON para evitar loops infinitos
     private User user;
    
+    
 }
