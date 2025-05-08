92823958e1e932d32e58ac6bb8e08ce4b6450513
Larissa Muniz <larissampmuniz@gmail.com>
2025-05-08 17:18:59 -0300

    feat: criar repositório de usuários com busca por e-mail

--- 1° SEMESTRE/Java/fin-money-api/src/main/java/br/com/fiap/fin_money_api/repository/UserRepository.java
+++ 1° SEMESTRE/Java/fin-money-api/src/main/java/br/com/fiap/fin_money_api/repository/UserRepository.java
@@ -1,15 +1,18 @@
 package br.com.fiap.fin_money_api.repository;
 
-import java.lang.StackWalker.Option;
+
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import br.com.fiap.fin_money_api.model.User;
 
+
+// define a interface UserRepository que estende JpaRepository para fornecer acesso a métodos prontos para manipulação de dados de usuários
+// JpaRepository é uma interface do Spring Data JPA que fornece métodos para operações CRUD e consultas em entidades
 public interface UserRepository extends JpaRepository<User, Long> {
+
     
+    // declara um método para buscar um usuário pelo e-mail
+    // o Spring Data JPA implementa esse método automaticamente com base no nome
     Optional<User> findByEmail(String email);
 
 }
